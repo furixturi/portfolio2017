4944a6f94b4ada67180f3186c2e9e6ef7ee2bf11
furixturi <alabebop@gmail.com>
2017-06-05 22:37:17 +0900

    build first react component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-console.log('My mini react webpack babel setup change');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "Xiaoli Shen | Portfolio 2017";
+
+ReactDOM.render(
+    <div>{title}</div>,
+    document.getElementById('app')
+);
 
 module.hot.accept();
