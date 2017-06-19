ba4d695a5a6551d9444cc26f6ae5297d7de6c75d
furixturi <alabebop@gmail.com>
2017-06-19 09:13:36 +0900

    navi js in app

---
+++ static/src/js/01-app.js
@@ -0,0 +1,45 @@
+/**
+ * 01-app.js
+ */
+; (function ($) {
+    // namespace
+    window.xiaoliPortfolio2017 = {
+        info: "Xiaoli Shen | Portfolio 2017"
+    };
+
+    $(function () {
+
+        var app = window.xiaoliPortfolio2017;
+        app.$components = {
+            nav : $('nav')
+        }
+
+        /* $('a[href="#projects"]').on('click', function(e){
+             e.preventDefault();
+         })
+     
+         app.navigateTo = function(){
+     
+         };*/
+
+//// navigation
+        $(document).on('nav_change', function (e, arg) {
+            app.onNavChange(arg);
+        });
+
+        app.onNavChange = function(target) {
+
+        }
+        
+//// open/close project detail
+        $(document).on('project_detail_open', function(e, arg) {
+            app.$components.nav.trigger('nav_hide');
+        });
+
+        $(document).on('project_detail_close', function(e, arg) {
+            app.$components.nav.trigger('nav_show');
+        });
+
+
+    });
+})($);
