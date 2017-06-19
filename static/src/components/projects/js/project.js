b32b3a1a09b1d6c41d2be42fb570d153599c3ca9
furixturi <alabebop@gmail.com>
2017-06-19 09:16:15 +0900

    project open close detail js and css animation

---
+++ static/src/components/projects/js/project.js
@@ -0,0 +1,24 @@
+$(function(){
+    var projects = $('#projects article');
+
+    $.each(projects, function(){
+        var $el = $(this),
+            $moreInfo = $el.find('a.more-info'),
+            $lessInfo = $el.find('a.less-info');
+
+        $moreInfo.on('click', function(e){
+            e.preventDefault();
+            $el.addClass('detail');
+            $(document).trigger('project_detail_open', [$el])
+        });
+
+        $lessInfo.on('click', function(e){
+            e.preventDefault();
+            $el.removeClass('detail');
+            $(document).trigger('project_detail_close', [$el])
+
+        });
+
+
+    })
+});
